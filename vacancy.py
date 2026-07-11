97e752a5f19ce9ec7482e5730f7930f657ad1cb6
Dana04 <dana04@example.com>
2026-07-11 12:44:02 +0500

    Update bot

--- vacancy.py
+++ vacancy.py
@@ -1,62 +1,45 @@
 import requests
-from bs4 import BeautifulSoup
 
 
 def get_vacancies():
 
-    url = "https://remoteok.com/remote-dev-jobs"
+    url = "https://api.hh.ru/vacancies"
 
     headers = {
-        "User-Agent": "Mozilla/5.0"
+        "HH-User-Agent": "EminMentor/1.0 (your_email@gmail.com)"
     }
 
-    try:
+    params = {
+        "host": "hh.kz",
+        "area": 40,
+        "text": "инвалидность удаленная гибкий график",
+        "per_page": 5
+    }
 
-        response = requests.get(
-            url,
-            headers=headers,
-            timeout=10
+    response = requests.get(
+        url,
+        params=params,
+        headers=headers,
+        timeout=10
+    )
+
+    if response.status_code != 200:
+        return f"❌ Ошибка {response.status_code}\n{response.text}"
+
+    data = response.json()
+
+    if not data["items"]:
+        return "❌ Вакансии не найдены."
+
+    result = "💼 Вакансии для людей с инвалидностью:\n\n"
+
+    for job in data["items"]:
+
+        result += (
+            f"💼 {job['name']}\n"
+            f"🏢 {job['employer']['name']}\n"
+            f"📍 {job['area']['name']}\n"
+            f"🔗 {job['alternate_url']}\n\n"
         )
 
-        if response.status_code != 200:
-            return "❌ Не удалось получить вакансии."
-
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        jobs = soup.find_all("tr", class_="job")
-
-        if not jobs:
-            return "❌ Вакансии не найдены."
-
-        result = "💼 Подходящие вакансии:\n\n"
-
-        count = 0
-
-        for job in jobs:
-
-            if count == 5:
-                break
-
-            title = job.get("data-position") or "Без названия"
-            company = job.get("data-company") or "Не указано"
-
-            link = job.get("data-href")
-
-            if link:
-                link = "https://remoteok.com" + link
-            else:
-                link = "https://remoteok.com"
-
-            result += (
-                f"🔹 {title}\n"
-                f"🏢 {company}\n"
-                f"🔗 {link}\n\n"
-            )
-
-            count += 1
-
-        return result
-
-    except Exception as e:
-
-        return f"❌ Ошибка: {e}"
+    return result
